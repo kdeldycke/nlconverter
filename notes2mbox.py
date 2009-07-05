9051dbf0d128656540b716fbac321a5109910fa7
hb <hb@localhost>
2009-07-05 11:28:19 +0200

    work on issues #2 and #3

--- notes2mbox.py
+++ notes2mbox.py
@@ -28,55 +28,135 @@ import win32com.client
 reAddressNotes = re.compile(r'CN=(.*?)\s+(.*?)\/OU=DGI\/OU=FINANCES\/O=GOUV\/C=FR', re.IGNORECASE)
 reAddressMail = re.compile(r'([a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,6})', re.IGNORECASE)
 
-#Helpers pour accéder à COM
-def get(doc, itemname):
-    return doc.GetItemValue(itemname)
+class NotesDocumentReader(object):
+    def __init__(self):
+        self.tempname = os.path.join(tempfile.gettempdir(), 'nlc.tmp')
 
-def get1(doc, itemname):
-    itemvalue = get(doc, itemname)
-    if len(itemvalue):
-        return itemvalue[0]
-    else :
-        return u''
+    def get(self, doc, itemname):
+        return doc.GetItemValue(itemname)
 
-def makeheader(value, charset = 'iso-8859-15'):
-    return email.header.Header(value, charset)
+    def getDocumentType(self, doc):
+        return self.get1(doc, 'Form')
 
-def header(doc, itemname):
-    return makeheader(get1(doc, itemname))
-
-def matchAddress(value):
-    res = reAddressNotes.search(value)
-    if res == None:
-        res = reAddressMail.search(value)
-        if res == None:
-            return value.lower()
+    def get1(self, doc, itemname):
+        itemvalue = self.get(doc, itemname)
+        if len(itemvalue):
+            return itemvalue[0]
         else :
-            return res.group(1)
-    else :
-        return u"%s.%s@dgfip.finances.gouv.fr" % (res.group(1).lower(), res.group(2).lower())
-        
-def addressHeader(doc, item):
-    items = get(doc, item)
-    return makeheader(",".join(map(matchAddress, items)))
+            return u''
 
-def messageHeaders(doc, m):
-    m['Subject'] = header(doc, "Subject")
-    m['From'] = addressHeader(doc, "From")
-    m['To'] = addressHeader(doc, "sendto")
-    m['Cc'] = addressHeader(doc, "copyto")
-    m['Date'] = get1(doc, "PostedDate")
-    if m['Date'] == u'':
-        m['Date'] = get1(doc, "DeliveredDate")
-    ccc = addressHeader(doc, "BlindCopyTo")
-    if ccc != u'':
-        m['Ccc'] = ccc
+    def checkDocumentType(self, doc):
+        return True
+        
+    def debug(self, doc):
+        for it in doc.Items:
+            try:
+                print it, doc.GetItemValue(it)
+            except:
+                print it, "!! can't display item value !!"
+
+    def matchAddress(value):
+        res = reAddressNotes.search(value)
+        if res == None:
+            res = reAddressMail.search(value)
+            if res == None:
+                return value.lower()
+            else :
+                return res.group(1)
+        else :
+            return u"%s.%s@dgfip.finances.gouv.fr" % (res.group(1).lower(), res.group(2).lower())
+
+    def listAttachments(self, doc):
+        return filter(lambda x : x != None and x != u'', self.get(doc, "$FILE"))
+
+    def hasAttachments(self, doc):
+        return len(self.listAttachments) > 0
+        
+    def extractAttachment(self, doc, f):
+        a = doc.GetAttachment(f)
+        #FIXME : tester le \xa0
+        a.ExtractFile(self.tempname)
+
+class NotesMemoReader(NotesDocumentReader):
+    def checkDocumentType(self, doc):
+        return self.getDocumentType(doc) == u'Memo'
+      
+class NotesDocumentConverter(NotesDocumentReader):
+    pass
+
+class NotesMemoToMimeConverter(NotesDocumentConverter):
+    charset = 'iso-8859-15'
+    
+    def stringToHeader(self, value):
+        return email.header.Header(value, self.charset)
+        
+    def header(self, doc, itemname):
+        return self.stringToHeader(self.get1(doc, itemname))
+    
+    def matchAddress(self, value):
+        res = reAddressNotes.search(value)
+        if res == None:
+            res = reAddressMail.search(value)
+            if res == None:
+                return value.lower()
+            else :
+                return res.group(1)
+        else :
+            return u"%s.%s@dgfip.finances.gouv.fr" % (res.group(1).lower(), res.group(2).lower())
+
+    def buildMailBody(self, doc):
+        return email.mime.text.MIMEText(mc.get1(doc, 'Body'), _charset=self.charset)
+
+    def addressHeader(self, doc, item):
+        items = self.get(doc, item)
+        return self.stringToHeader(",".join(map(self.matchAddress, items)))
+    
+    def messageHeaders(self, doc, m):
+        m['Subject'] = self.header(doc, "Subject")
+        m['From'] = self.addressHeader(doc, "From")
+        m['To'] = self.addressHeader(doc, "sendto")
+        m['Cc'] = self.addressHeader(doc, "copyto")
+        m['Date'] = self.get1(doc, "PostedDate")
+        if m['Date'] == u'':
+            m['Date'] = self.get1(doc, "DeliveredDate")
+        ccc = self.addressHeader(doc, "BlindCopyTo")
+        if ccc != u'':
+            m['Ccc'] = ccc
+        m['User-Agent'] = self.header(doc, "$Mailer")
+        m['Message-ID'] = self.header(doc, "$MessageID")
+
+    def buildAttachment(self, doc, f):
+        self.extractAttachment(doc, f)
+        fp = open(self.tempname, 'rb')
+        msg = email.mime.base.MIMEBase('application', 'octet-stream')
+        msg.set_payload(fp.read())
+        fp.close()
+        encoders.encode_base64(msg)
+        msg.add_header('Content-Disposition', 'attachment', filename=f.encode(self.charset))
+        return msg
+
+    def buildMessage(self, doc):
+        main = self.buildMailBody(doc)
+        
+        #files
+        files = self.listAttachments(doc)
+        if len(files) > 0 :
+            m = email.mime.multipart.MIMEMultipart(charset=self.charset)
+            m.set_charset(self.charset)
+            self.messageHeaders(doc, m)
+            m.attach(main)
+            for f in files :
+                msg = self.buildAttachment(doc, f)
+                m.attach(msg)
+        else:
+            m = main
+            self.messageHeaders(doc, m)
+        return m
 
 #Constantes
 notesPasswd = "foobar"
 notesNsfPath = "C:\\archive.nsf"
 mailboxName = notesNsfPath+".mbox"
-tempdir = tempfile.mkdtemp('_nlconverter')
 
 #Connection à Notes
 session = win32com.client.Dispatch(r'Lotus.NotesSession')
@@ -93,51 +173,18 @@ print "Nombre de documents :", all.Count
 c = 0 #compteur de documents
 e = 0 #compteur d'erreur à la conversion
 
+mc = NotesMemoToMimeConverter()
+
 doc = all.GetFirstDocument()
-while doc and c < 99999 and e < 5:
+while doc and c < 200 and e < 99999:
     try:
-        main = email.mime.text.MIMEText(doc.GetItemValue("Body")[0], _charset='iso-8859-15')
-        
-        #files
-        files = filter(lambda x : x != None and x != u'', get(doc, "$FILE"))
-        if len(files) > 0 :
-            m = email.mime.multipart.MIMEMultipart(charset='iso-8859-15')
-            m.set_charset('iso-8859-15')
-            messageHeaders(doc, m)
-            m.attach(main)
-            for f in files :
-                a = doc.GetAttachment(f)
-                if a == None :
-                    continue
-                fpath = os.path.join(tempdir, f)
-                a.ExtractFile(fpath)
-                ctype, encoding = mimetypes.guess_type(fpath)
-                if ctype is None or encoding is not None:
-                    # No guess could be made, or the file is encoded (compressed), so
-                    # use a generic bag-of-bits type.
-                    ctype = 'application/octet-stream'
-                maintype, subtype = ctype.split('/', 1)
-                fp = open(fpath, 'rb')
-                msg = email.mime.base.MIMEBase(maintype, subtype)
-                msg.set_payload(fp.read())
-                fp.close()
-                encoders.encode_base64(msg)
-
-                msg.add_header('Content-Disposition', 'attachment', filename=f.encode('utf-8'))
-                m.attach(msg)
-                os.remove(fpath)
-        else:
-            m = main
-            messageHeaders(doc, m)
-
+        m = mc.buildMessage(doc)
         mbox.add (m)
         
     except Exception as ex:
         e += 1 #compte les exceptions
-        print "-----------Exception, message %d" % c
-        print ex
-        for it in doc.Items:
-            print it, doc.GetItemValue(it)
+        print "\n--Exception for message %d (%s)" % (c, ex)
+        mc.debug(doc)
 
     finally:
         doc = all.GetNextDocument(doc)
@@ -145,5 +192,4 @@ while doc and c < 99999 and e < 5:
 
 print "Exceptions a traiter manuellement:", e
 mbox.close()
-os.rmdir(tempdir)
 #FIXME : session OLE a cloturer
